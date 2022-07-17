681c67754d62ff34cdf62dbcb0eb54673008ea52
alpysaruzhan <alpysaruzan4@gmail.com>
2022-07-17 18:02:42 +0600

    1,2 zadaniye new

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,12 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+interface skill {
+  id: number;
+  name: string;
+  template: string;
+}
+
+
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
   styleUrls: ['./second.component.scss']
 })
-export class SecondComponent implements OnInit {
-
+export class SecondComponent {
+  skills: skill[] = [
+    {
+      id:1,
+      name: 'опыт работы с HTML5, CSS3, JS;',
+      template: 'bold'
+    },
+    {
+      id: 2, 
+      name: 'знание JavaScript/JQuery;',
+      template: 'bold'
+    },
+    {
+      id: 3,
+      name: 'опыт адаптивной верстки;',
+      template: 'bold'
+    },
+    {
+      id:4,
+      name: 'опыт создания HTML-страницы сайта на основе дизайн-макетов;',
+      template: 'bold'
+    },
+    {
+      id: 5,
+      name: 'знание фреймворков',
+      template: 'italic'
+    }
+  ];
+  expression = true;
   constructor() { }
 
   ngOnInit(): void {
